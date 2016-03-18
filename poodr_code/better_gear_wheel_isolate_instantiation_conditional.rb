fde43d2abbc7b913fe92df33334dc663d54e5300
Fiona Sanggang <fsanggang@gmail.com>
2016-03-18 14:23:11 +1300

    Managing dependencies: Another example of isolated instantiation
    The creation of a new Wheel occurs only when it doesn't already exist, in #wheel
    
    1. #gear_inches now doesn't need to know about Wheel, it only expects
       something that responds to #diameter.
    
    2. Gear's dependency on Wheel is made explicit

---
+++ poodr_code/better_gear_wheel_isolate_instantiation_conditional.rb
@@ -0,0 +1,44 @@
+class Gear
+
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog       = cog
+    @rim       = rim
+    @tire      = tire
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+  def wheel
+    @wheel ||= Wheel.new(rim, tire)
+  end
+
+end
+
+class Wheel
+
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim  = rim
+    @tire = tire
+  end
+
+  # YOU HAD ONE JOB! And that is to calculate wheel diameter
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+
+end
