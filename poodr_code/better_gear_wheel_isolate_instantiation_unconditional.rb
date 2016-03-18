945f610073dc6513a0b09cae015983125d25af29
Fiona Sanggang <fsanggang@gmail.com>
2016-03-18 14:17:31 +1300

    Managing dependencies: An example of isolated instantiation
    The (unconditional) creation of a new Wheel occurs only when a new Gear is
    created. Benefits of this approach:
    
    1. #gear_inches now doesn't need to know about Wheel, it only expects
       something that responds to #diameter.
    
    2. Gear's dependency on Wheel is made explicit

---
+++ poodr_code/better_gear_wheel_isolate_instantiation_unconditional.rb
@@ -0,0 +1,39 @@
+class Gear
+
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog       = cog
+    @wheel     = Wheel.new(rim, tire)
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+end
+
+class Wheel
+
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim  = rim
+    @tire = tire
+  end
+
+  # YOU HAD ONE JOB! And that is to calculate wheel diameter
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+
+end
