fc551e4e6bc3cc16f2c418e0e4c0743998b4465e
Fiona Sanggang <fsanggang@gmail.com>
2016-03-21 11:01:51 +1300

    Managing dependencies: An example of isolated external messages
    Gear sends messages to self, as well as to Wheel i.e. wheel.diameter in
    
    This embedded, external dependency is especially bad if #gear_inches is a
    complex method, as things are more likely to break whenever a change is made.
    Much better, then, to isolate the wheel.diameter in a method of its own.
    The result?
    
    1. #gear_inches only sends messages to self
    
    2. Also has a DRY-ing effect

---
+++ poodr_code/better_gear_wheel_isolate_external_messages.rb
@@ -0,0 +1,45 @@
+class Gear
+
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring, cog, wheel)
+    @chainring = chainring
+    @cog       = cog
+    @wheel     = wheel
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    # a few lines of scary math
+    foo = some_intermediate_result * diameter
+    # a few lines of scary math
+  end
+
+  def diameter
+    wheel.diameter
+  end
+
+end
+
+class Wheel
+
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim  = rim
+    @tire = tire
+  end
+
+  # YOU HAD ONE JOB! And that is to calculate wheel diameter
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+
+end
