b3b2f7058c0e0d34b635a55e5db7f281c83bd652
Fiona Sanggang <fsanggang@gmail.com>
2016-03-23 14:36:14 +1300

    Modules: Schedule at start.

---
+++ poodr_code/modules/modules.rb
@@ -0,0 +1,15 @@
+class Schedule
+
+  def scheduled?(target, starts, ends)
+    # etc
+  end
+
+  def add(target, starts, ends)
+    # etc
+  end
+
+  def remove(target, starts, ends)
+    # etc
+  end
+
+end
